8c88b1e596291151c601a78a4a59062f18592055
Gabriel Ichiro <gabriel582084@gmail.com>
2022-11-24 09:23:39 -0400

    added second test

--- src/oracion.spec.js
+++ src/oracion.spec.js
@@ -1,6 +1,12 @@
-import contarPalabras from "./oracion"
+import {contarPalabras, contarApariciones} from "./oracion"
 describe("contar palabras", () => {
   it("deberia devolver cuantas palabras tiene mi string", () => {
     expect(contarPalabras("hello world")).toEqual(2);
+  });
+});
+
+describe("ver apariciones de una palabra", () => {
+  it("deberia devolver cuantas veces aparece una palabra en string", () => {
+    expect(contarApariciones("mi password es password")).toEqual(2);
   });
 });
