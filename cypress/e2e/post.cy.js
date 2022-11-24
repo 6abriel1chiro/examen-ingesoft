363434e1934cbc872a4d5b9e5927224ca88e89e4
Gabriel Ichiro <gabriel582084@gmail.com>
2022-11-24 09:05:55 -0400

    empieza criterio 2

--- cypress/e2e/post.cy.js
+++ cypress/e2e/post.cy.js
@@ -5,4 +5,12 @@ describe("twitter pirata", () => {
     cy.get("#post-button").click();
     cy.get("#posts-div").should("contain", "hola mundo");
   });
+
+
+  it("muestra las publicaciones que ha hecho el usuario", () => {
+    cy.visit("/");
+    cy.get("#post-txt").type("");
+    cy.get("#post-button").click();
+    cy.get("#error-div").should("contain", "no se puede ingresar un post sin texto");
+  });
 });
--- src/presenter.js
+++ src/presenter.js
@@ -2,11 +2,14 @@
 const text = document.querySelector("#post-txt");
 const form = document.querySelector("#post-form");
 const div = document.querySelector("#posts-div");
+const error = document.querySelector("#error-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+
   const publicacion = text.value;
 
   div.innerHTML = "<p>" + publicacion + "</p>";
+
 });
