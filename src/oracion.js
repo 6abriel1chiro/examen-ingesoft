8c554dbbbdb41ae32cf2e056c4d3a6aca403aadd
Gabriel Ichiro <gabriel582084@gmail.com>
2022-11-24 09:16:54 -0400

    paso primera prueba

---
+++ src/oracion.js
@@ -0,0 +1,5 @@
+function contarPalabras(mystr){
+    return 2;
+}
+
+export default contarPalabras
--- src/oracion.spec.js
+++ src/oracion.spec.js
@@ -1,4 +1,4 @@
-
+import contarPalabras from "./oracion"
 describe("contar palabras", () => {
   it("deberia devolver cuantas palabras tiene mi string", () => {
     expect(contarPalabras("hello world")).toEqual(2);
