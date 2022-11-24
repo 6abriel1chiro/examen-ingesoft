c56ad496e8881035353b60b2104e616bfcfc2406
Gabriel Ichiro <gabriel582084@gmail.com>
2022-11-24 09:29:05 -0400

    added third test

--- src/oracion.spec.js
+++ src/oracion.spec.js
@@ -9,4 +9,10 @@ describe("ver apariciones de una palabra", () => {
   it("deberia devolver cuantas veces aparece una palabra en string", () => {
     expect(contarApariciones("mi password es password","password")).toEqual(2);
   });
+
+
+  it("deberia no deberia diferenciar entre mayuscula y minuscula", () => {
+    expect(contarApariciones("mi PASSWORD es passWord, que mala password","PassWord")).toEqual(3);
+  })
+
 });
